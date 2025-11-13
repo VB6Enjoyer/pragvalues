392b117e75d4358c7183625b0732e190f02d839c
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-11-12 22:46:32 -0300

    bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configure the output to be a static export.
+  output: 'export',
+
+  // Configure the base path for the project.
+  // This is the name of the GitHub repository.
+  basePath: process.env.NODE_ENV === 'production' ? '/pragvalues' : '',
+
   typescript: {
     ignoreBuildErrors: true,
   },
